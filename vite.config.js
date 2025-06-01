d0c8d0a80be59c51bfcb27fb345d01d993a6f652
seonetwork24 <166035562+seonetwork24@users.noreply.github.com>
2025-06-01 21:11:48 +0600

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
+});
